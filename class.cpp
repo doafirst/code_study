69872c35a962efddf9b62ac4b78a6a9dc7e0afea
Harry Lin <harry.lin@savitech.co>
2020-08-26 17:29:57 +0800

    [UPDATE] - Add function in the class and this->

--- class.cpp
+++ class.cpp
@@ -2,9 +2,40 @@
 
 using namespace std;
 
+class Account {
+	public:
+		Account(int a,int b ,int c);
+		void PPP();
+	private:
+		int a;
+		int b;
+		int c;
+
+};
+
+//Account::Account(int ddd)
+//{
+//	cout<<"I am DDD"<<endl;
+//	cout<<ddd<<endl;
+//}
+
+Account::Account(int a,int b ,int c)
+{
+	this->a = a;
+	this->b = b;
+	//this->c = c;
+}
+
+void Account::PPP()
+{
+        cout<<"I am PPP"<<endl;
+	cout<<this->a<<endl;
+}
 
 int main(){
+	Account act = {40, 20 ,30};
 	cout<< endl;
 	cout<<"Hello World"<<endl;
+	act.PPP();
 	return 0;
 }

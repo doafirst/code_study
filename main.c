b936ef764596c106db0a35c85cb8a05abb98baf8
Harry Lin <harry.lin@savitech.co>
2020-08-28 17:09:18 +0800

    [UPDATE] - Find out argc and argv functions
    [DESCRIPTION]
    String in array:
      Array stored those pointer.
      Those pointer should be malloc. Then put the string in.
    Double array:
       Declare array like squart memory mapping

--- main.c
+++ main.c
@@ -8,10 +8,28 @@ int test(char ** s){
 }
 
 int main(int argc ,char **argv){
-	char a[2][10]={"hello","OK"};
+	//char a[2][10]={"hello","OK"};
 	//s = test();
+	//printf("%s %s \n",a[0],a[1]);
+	char *a[2];
+	printf("================String In Array================\n");
+	a[0] = malloc(strlen("HAHA")+1);
+	a[1] = malloc(strlen("IM OOK")+1);
+	a[0] = "HAHA";
+	a[1] = "IM OOK";
 	printf("%s %s \n",a[0],a[1]);
+	printf("a[0] -> %x %x \n",a,a[0]);
+	printf("a[1] -> %x %x \n",a+1,a[1]);
 
+	printf("================Double Array================\n");
+	char b[10][20];
+	printf("b = %x\n",b);
+	printf("b[0] = %x\n",b[0]);
+	printf("&b[0][1] = %x\n",&b[0][1]);
+	printf("&b[0][19] = %x\n",&b[0][19]);
+	printf("b[1] = %x\n",b[1]);
+	printf("================ARGC ARGC================\n");
+	
 	printf("addr line1 = %x %x\n",argv,argv+1);
 	printf("addr line2 = %x %x\n",*argv,*(argv+1));
 	printf("argc = %d\n",argc);
